7fa0e5128fadf0ca34947145237fca2ba57974f2
Skylar <dorienvermeulen82@gmail.com>
2024-11-27 09:53:41 +0100

    feat: Create saveReview.php to make adding reviews possible through AJAX (2/2)

---
+++ saveReview.php
@@ -0,0 +1,35 @@
+<?php
+
+    include_once(__DIR__."/Review.php");
+
+    if(!empty($_POST)) {
+        $r = new Review();
+        
+        $rating = $_POST['rating'];
+        $text = $_POST['text']; 
+        $product_id = $_POST['product_id'];
+        $user_id = $_POST['user_id'];
+
+        $r = new Review();
+        $r->setProductId($product_id);  
+        $r->setText($text);              
+        $r->setRating($rating);          
+        $r->setUserId($user_id);
+
+        $r -> save();
+
+        $response = [
+            "status" => "success",
+            "message" => "Review saved, thank you",
+            "text" => htmlspecialchars($r->getText()),
+            "rating" => htmlspecialchars($r->getRating())
+        ];
+
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid request method or empty data']);
+            http_response_code(400); 
+        }
+    
+?>
