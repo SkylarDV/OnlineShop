74c8de8d24faabc717b6a6491865e79767c53a1d
Skylar <dorienvermeulen82@gmail.com>
2024-11-29 11:53:13 +0100

    feat: Database connected to the Azure server instead of localhost

--- Db.php
+++ Db.php
@@ -2,15 +2,18 @@
     class Db {
         private static $conn;
         const SETTINGS = [
-            "user" => "root", //r1003207
-            "password" => "root", //e5s1b8#ZeftS
-            "host" => "localhost", //onlinestore.mysql.database.azure.com
-            "db" => "onlinestore"
+            "user" => "r1003207", 
+            "password" => "e5s1b8#ZeftS", 
+            "host" => "onlinestore.mysql.database.azure.com", 
+            "db" => "onlinestore",
+            "ssl_ca" => __DIR__ . "/cacert.pem"
         ];
+
 
         public static function getConnection(){
             if (self::$conn === null) {
-                self::$conn = new PDO("mysql:host=".self::SETTINGS["host"].";dbname=".self::SETTINGS["db"]."",self::SETTINGS["user"],self::SETTINGS["password"]);
+                $options[PDO::MYSQL_ATTR_SSL_CA] = self::SETTINGS['ssl_ca'];
+                self::$conn = new PDO("mysql:host=".self::SETTINGS["host"].";dbname=".self::SETTINGS["db"]."",self::SETTINGS["user"],self::SETTINGS["password"], $options);
                 // [PDO::MYSQL_ATTR_SSL_CA => __DIR__."/CA.pem"]
                 return self::$conn;
             }
