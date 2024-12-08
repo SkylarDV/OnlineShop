3756fdd16bb52e20c25476737502e904b56ea42a
SkylarDV <dorienvermeulen82@gmail.com>
2024-12-08 19:00:00 +0100

    fix(?): changed page names in the tab for aesthetic purposes

--- cart.php
+++ cart.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Your Cart</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
