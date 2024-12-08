1ab1866f44c2e97bc7588f5b39ff2c5066ce960a
SkylarDV <dorienvermeulen82@gmail.com>
2024-12-08 18:21:48 +0100

    fix: last try to make the env work before i revert back to leaving everything just in the file

--- Db.php
+++ Db.php
@@ -2,25 +2,25 @@
     class Db {
         private static $conn;
 
-        private string $user = getenv("user");
-        private string $password = getenv("password");
-        private string $host = getenv("host");
-        private string $db = getenv("db");
-
-        
-        const SETTINGS = [
-            "user" => $user, 
-            "password" => $password, 
-            "host" => $host, 
-            "db" => $db,
-            "ssl_ca" => __DIR__ . "/cacert.pem"
-        ];
-
 
         public static function getConnection(){
+            
+            $user = getenv("user");
+            $password = getenv("password");
+            $host = getenv("host");
+            $db = getenv("db");
+
+            $SETTINGS = [
+                "user" => $user, 
+                "password" => $password, 
+                "host" => $host, 
+                "db" => $db,
+                "ssl_ca" => __DIR__ . "/cacert.pem"
+            ];
+
             if (self::$conn === null) {
-                $options[PDO::MYSQL_ATTR_SSL_CA] = self::SETTINGS['ssl_ca'];
-                self::$conn = new PDO("mysql:host=".self::SETTINGS["host"].";dbname=".self::SETTINGS["db"]."",self::SETTINGS["user"],self::SETTINGS["password"], $options);
+                $options[PDO::MYSQL_ATTR_SSL_CA] = $SETTINGS['ssl_ca'];
+                self::$conn = new PDO("mysql:host=".$SETTINGS["host"].";dbname=".$SETTINGS["db"]."",$SETTINGS["user"],$SETTINGS["password"], $options);
                 // [PDO::MYSQL_ATTR_SSL_CA => __DIR__."/CA.pem"]
                 return self::$conn;
             }
